62d774070eef4b000d10e64d6342fd9cec624cfd
jl3387 <jl3387@cornell.edu>
2015-10-28 20:44:52 -0400

    add button

--- js/pi_graph.js
+++ js/pi_graph.js
@@ -36,9 +36,15 @@ var color = d3.scale.ordinal()
   .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
 function randomData (){
-  var labels = color.domain();
-  return labels.map(function(label){
-    return { label: label, value: Math.random() }
+    var labels = color.domain();
+    labels = ["ddd"];
+
+    
+
+
+    
+    return labels.map(function(label){
+      return { label: label, value: Math.random() }
   });
 }
 
